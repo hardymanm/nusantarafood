b73d4994e63c045afd8366e57b73f385a01a96f0
Amir Hamzah Bujang <amirh.bujang@gmail.com>
2022-08-20 18:08:22 +0800

    Add search by title in django admin (Document and Recipe model)

--- nusantarafood-django/server/nfo/admin.py
+++ nusantarafood-django/server/nfo/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from nfo import models
 
+class DocumentAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
 admin.site.register(models.Judge)
-admin.site.register(models.Document)
+admin.site.register(models.Document, DocumentAdmin)
 admin.site.register(models.Dataset)
 admin.site.register(models.FoodCategory)
-admin.site.register(models.Recipe)
+admin.site.register(models.Recipe, RecipeAdmin)
 admin.site.register(models.LdaModel)
 admin.site.register(models.Word)
 admin.site.register(models.TabelEvaluation)
