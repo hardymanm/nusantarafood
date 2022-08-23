1cb9799c1f8a9d8d0c3e6c1122c16bdff1b62dd4
Amir Hamzah Bujang <amirh.bujang@gmail.com>
2022-08-24 00:44:12 +0800

    remove unused code - not using django rest framework for now

--- nusantarafood-django/server/nfo/models.py
+++ nusantarafood-django/server/nfo/models.py
@@ -5,11 +5,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-
 
 class Document(models.Model):
     title = models.CharField(max_length=255)
