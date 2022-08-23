fced5135557afd7586a6cd5cc5dcd2516998328a
Amir Hamzah Bujang <amirh.bujang@gmail.com>
2022-08-24 00:40:11 +0800

    fix bug - unused import

--- nusantarafood-django/server/nfo/models.py
+++ nusantarafood-django/server/nfo/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
-from rest_framework.authtoken.models import Token
-
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
