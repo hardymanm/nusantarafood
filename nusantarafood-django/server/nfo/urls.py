b8bc78547ae5bab9507758c21c9a493ead381af9
Amir Hamzah Bujang <amirh.bujang@gmail.com>
2022-08-23 23:34:50 +0800

    Update ui

--- nusantarafood-django/server/nfo/urls.py
+++ nusantarafood-django/server/nfo/urls.py
@@ -14,12 +14,10 @@ urlpatterns = [
     path('judge-wordnet/<int:pk>', views.JudgeWordnetInstruction.as_view(), name='judge-wordnet-instruction'),
     path('judge-wordnet/<int:pk>/item/<int:page>', views.judge_wordnet_item, name='judge-wordnet-item'),
     path('judge-wordnet/<int:pk>/done', views.JudgeWordnetDone.as_view(), name='judge-wordnet-done'),
-    
-    
+     
     path('judge-wiki/<int:pk>', views.JudgeWikiInstruction.as_view(), name='judge-wiki-instruction'),
     path('judge-wiki/<int:pk>/item/<int:page>', views.judge_wiki_item, name='judge-wiki-item'),
     path('judge-wiki/<int:pk>/done', views.JudgeWikiDone.as_view(), name='judge-wiki-done'),
-    
         
     path('judge-tabel/<int:pk>', views.JudgeTabelInstruction.as_view(), name='judge-tabel-instruction'),
     path('judge-tabel/<int:pk>/item/<int:page>', views.judge_tabel_item, name='judge-tabel-item'),
