55edf0e0948e38728d48131fced5a253bc94e245
Amir Hamzah Bujang <amirh.bujang@gmail.com>
2023-06-21 00:30:10 +0800

    Added pyLDAvis to create LDA visualization (in manage dataset webpage)

--- nusantarafood-gui/lda.py
+++ nusantarafood-gui/lda.py
@@ -2,6 +2,7 @@ import re
 import nltk
 from nltk.corpus import wordnet
 import gensim
+import pyLDAvis, pyLDAvis.gensim_models
 from sklearn.feature_extraction.text import CountVectorizer
 
 DEFAULT_PASSES = 20
@@ -14,54 +15,30 @@ nltk.download('omw')
 nltk.download('omw-1.4')
 
 
-def load_stopwords(filename):
-    stopwords = []
-    with open(filename, 'r') as f:
-        stopwords = f.read().splitlines()
-
-    return stopwords
-
-
-def remove_long_sentences(content, max_word=20):
-    short_sentences = []
-    for sentence in content.split('.'):
-        if len(re.findall('\w+', sentence)) > max_word:
-            short_sentences.append(sentence)
-
-    return '. '.join(short_sentences)
-
-
-def remove_number(content):
-    return re.sub('\d+', '', content)
-
-
-def remove_stopwords(content, stopwords):
-    # @todo: has effect removing period. therefore we lose sentence
-    # @todo: CountVectorizer has stop_words param. Use that instead?
-    tokens = re.findall('\w+', content)
-    clean = []
-    for token in tokens:
-        if token in stopwords:
-            clean.append(token)
-
-    return ' '.join(clean)
-
-
 def make_LDAmodel(contents, num_topics, passes):
     vect = CountVectorizer(token_pattern='(?u)\\b\\w\\w\\w+\\b')
     X = vect.fit_transform(contents)
     corpus = gensim.matutils.Sparse2Corpus(X, documents_columns=False)
 
     id_map = dict((v, k) for k, v in vect.vocabulary_.items())
+    word_map = dict((k, v) for k, v in vect.vocabulary_.items())
 
-    return gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, passes=passes, random_state=0, id2word=id_map,
-                                           chunksize=100, alpha='auto', per_word_topics=True, minimum_probability=1E-9)
+    model = gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, passes=passes, random_state=0,
+                                            id2word=id_map,
+                                            chunksize=100, alpha='auto', per_word_topics=True, minimum_probability=1E-9)
+
+    d = gensim.corpora.Dictionary()
+    d.id2token = id_map
+    d.token2id = word_map
+    model_data = pyLDAvis.gensim_models.prepare(model, corpus, d)
+
+    return model, model_data
 
 
 def get_word_list(lda_model, num_words):
     result = []
     for i, topic in lda_model.show_topics(formatted=False, num_words=num_words):
-        print('Topic:', i+1)
+        print('Topic:', i + 1)
         for word, probability in topic:
             print(word.ljust(20), probability)
             result.append(word)
