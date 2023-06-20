c5ccb79fce00af9f330007c53b11c888919ee8ef
Amir Hamzah Bujang <amirh.bujang@gmail.com>
2023-06-20 14:12:32 +0800

    Added fields for scraping

--- nusantarafood-django/nfo/models.py
+++ nusantarafood-django/nfo/models.py
@@ -131,6 +131,7 @@ class Document(models.Model):
     title = models.CharField(max_length=255)
     title_clean = models.CharField(max_length=255)
     url = models.CharField(max_length=255, null=True, blank=True)
+    raw_content = models.TextField(null=True, blank=True)
     content = models.TextField()
 
     # wikipedia definition
